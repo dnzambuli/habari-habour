bd7b83ba359b51587bda8a152b334ff005351a9c
dnzambuli <damunzandm@outlook.com>
2024-08-13 18:48:16 +0300

    modify the app factory

--- app/__init__.py
+++ app/__init__.py
@@ -1,39 +1,43 @@
 import os
-
 from flask import Flask
 from dotenv import load_dotenv
 
-def create_app(test_config = None):
-    '''
-    create_app is an application factory
+from .db import db
 
-    - app -> instance of Flask
-    - __name__ -> current python module
+def create_app(test_config=None):
     '''
+    Create and configure an instance of the Flask application.
+
+    Args:
+    - test_config (dict, optional): Configuration settings to use for testing.
+
+    Returns:
+    - app: Instance of Flask configured with database and routes.
+    '''
+    # Load environment variables
+    load_dotenv()
 
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-        SECRET_KEY = 'dev',
-        SQLALCHEMY_DATABASE_URI=f'mysql+mysqlconnector://{os.environ.get("MYSQL_USER")}:{os.environ.get("MYSQL_PASSWORD")}@{os.environ.get("MYSQL_HOST")}/{os.environ.get("MYSQL_DATABASE")}',
-        SQLALCHEMY_TRACK_MODIFICATIONS=False  # Optional for performance optimization
-        )
+        SECRET_KEY='dev',
+        SQLALCHEMY_DATABASE_URI=f'mysql+mysqlconnector://{os.environ["MYSQL_USER"]}:{os.environ["MYSQL_PASSWORD"]}@{os.environ["MYSQL_HOST"]}/{os.environ["MYSQL_DATABASE"]}',
+        SQLALCHEMY_TRACK_MODIFICATIONS=False
+    )
 
-    db = SQLAlchemy(app)
+    db.init_app(app)
 
     if test_config is None:
-        app.config.from_pyfile("config.py", silent = True)
+        app.config.from_pyfile("config.py", silent=True)
     else:
-        app.config.from_mapping(test_config)
+        app.config.update(test_config)
     
     try:
-        os.makedirs(app.instance_path)
+        os.makedirs(app.instance_path, exist_ok=True)  # exist_ok=True makes the OSError handling redundant
     except OSError:
         pass
-
 
     @app.route('/hello')
     def hello():
         return 'Hello, World!'
-    
 
-    return app 
+    return app
